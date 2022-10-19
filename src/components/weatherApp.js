440b3207c3dcd2b55a0714b720568b5b6cc7a0b5
cristobal <cristobal.paez.pd@gmail.com>
2022-10-19 11:16:46 -0500

    proyecto final

--- src/components/weatherApp.js
+++ src/components/weatherApp.js
@@ -1,6 +1,8 @@
 import{useEffect, useState} from "react";
 import WeatherForm from "./weatherForm";
 import WeatherMainInfo from "./weatherMainInfo";
+import styles from './weatherApp.module.css';
+import Loading from "./loading";
 export default function WeatherApp(){
         const [weather, setWeather]=useState(null);
 
@@ -19,8 +21,10 @@ export default function WeatherApp(){
                 `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
                 );
             const json = await request.json();
-
-            setWeather(json);
+            setTimeout(() =>{
+                setWeather(json);
+            }, 2000);
+            
 
             console.log(json);
         }catch(error){
@@ -32,8 +36,9 @@ export default function WeatherApp(){
         setWeather(null);
         loadInfo(city);
     }
-    return <div>
+    return <div className={styles.weatherContainer}>
         <WeatherForm onChangeCity={handleChangeCity}/>
-        <WeatherMainInfo weather={weather} />
+        {weather ? <WeatherMainInfo weather={weather} /> : <Loading/> }
+        
     </div>
 }
